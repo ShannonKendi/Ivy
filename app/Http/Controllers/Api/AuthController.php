3140bcc1afdb1132af5868ac9fa2b35a1d48c69a
RichardJesse <jesse.richard@strathmore.edu>
2024-11-23 00:25:36 +0300

    began work on the apis

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends BaseController
+{
+
+ 
+}
